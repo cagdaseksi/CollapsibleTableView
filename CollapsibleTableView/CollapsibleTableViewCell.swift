f9bfd98cf475f56f939e3623b277673ad6c99c49
cagdaseksi <cagdas.eksi@hotmail.com>
2019-02-21 14:29:27 +0300

    Initial commit

---
+++ CollapsibleTableView/CollapsibleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CollapsibleTableViewCell.swift
+//  CollapsibleTableView
+//
+//  Created by MAC on 21.02.2019.
+//  Copyright © 2019 cagdaseksi. All rights reserved.
+//
+
+import UIKit
+
+class CollapsibleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CollapsibleTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollapsibleTableView
+//
+//  Created by MAC on 21.02.2019.
+//  Copyright © 2019 cagdaseksi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
